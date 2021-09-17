c4a8377ba6c9e4676010d2eb1857ea2691e0f096
Shaan Alam <shaanalam369@gmail.com>
2021-09-17 14:44:30 +0530

    solved single element in a array question on leetcode

---
+++ arrays/medium/Single_Element_in_a_Sorted_Array.java
@@ -0,0 +1,28 @@
+package medium;
+
+import java.util.ArrayList;
+
+public class Single_Element_in_a_Sorted_Array {
+  public int singleNonDuplicate(int[] nums) {
+
+    ArrayList<Integer> list = new ArrayList<>();
+    int result = 0;
+    int sum = 0;
+    int expectedSum = 0;
+
+    for (int x : nums) {
+      if (!list.contains(x)) {
+        list.add(x);
+      }
+
+      sum += x;
+    }
+
+    for (int x : list) {
+      expectedSum += 2 * x;
+    }
+
+    return expectedSum - sum;
+
+  }
+}

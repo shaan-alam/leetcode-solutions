faa3f95437f0f3c028d62e212bca58df301eae66
Shaan Alam <shaanalam369@gmail.com>
2021-09-26 11:42:06 +0530

    updated find first and last index question

--- arrays/medium/Find_First_and_Last_Position_Of_Elements_In_Sorted_Array.java
+++ arrays/medium/Find_First_and_Last_Position_Of_Elements_In_Sorted_Array.java
@@ -1,59 +1,42 @@
-/* Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
-
-If target is not found in the array, return [-1, -1].
-
-You must write an algorithm with O(log n) runtime complexity.
-
-Example 1:
-
-Input: nums = [5,7,7,8,8,10], target = 8
-Output: [3,4]
-
-Example 2:
-
-Input: nums = [5,7,7,8,8,10], target = 6
-Output: [-1,-1]
-
-Example 3:
-
-Input: nums = [], target = 0
-Output: [-1,-1]
-
- 
-
-Constraints:
-
-    0 <= nums.length <= 105
-    -109 <= nums[i] <= 109
-    nums is a non-decreasing array.
-    -109 <= target <= 109
-
-*/
 
 package medium;
 
-class Find_First_and_Last_Position_Of_Elements_In_Sorted_Array {
+/* 
+  Link - https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+*/
+public class Find_First_and_Last_Position_Of_Elements_In_Sorted_Array {
   public int[] searchRange(int[] nums, int target) {
-    int firstIndex = -1;
-    int lastIndex = -1;
+    int[] result = { -1, -1 };
 
-    int[] result = new int[2];
+    result[0] = bsearch(nums, target, true);
+    result[1] = bsearch(nums, target, false);
 
-    if (nums.length == 1 && nums[0] == target) {
-      firstIndex = lastIndex = 0;
-    } else {
-      for (int i = 0; i < nums.length; i++) {
-        if (firstIndex == -1 && target == nums[i]) {
-          firstIndex = i;
-        }
-        if (target == nums[i] && firstIndex != -1) {
-          lastIndex = i;
+    return result;
+  }
+
+  public int bsearch(int[] arr, int target, boolean isFromStart) {
+    int start = 0;
+    int end = arr.length - 1;
+    int result = -1;
+
+    while (start <= end) {
+      int mid = (start + end) / 2;
+
+      if (target > arr[mid]) {
+        start = mid + 1;
+      } else if (target < arr[mid]) {
+        end = mid - 1;
+      } else {
+        result = mid;
+
+        if (isFromStart) {
+          end = mid - 1;
+        } else {
+          start = mid + 1;
         }
       }
     }
 
-    result[0] = firstIndex;
-    result[1] = lastIndex;
     return result;
   }
 }

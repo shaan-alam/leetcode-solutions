c2f5d9c97c6a83ceb8c42776b8f0e53962f410c9
Shaan Alam <shaanalam369@gmail.com>
2021-09-26 09:49:09 +0530

    updated search index position question. solved using O(logn) complexity

--- arrays/easy/Search_Insert_Position.java
+++ arrays/easy/Search_Insert_Position.java
@@ -8,24 +8,22 @@ public class Search_Insert_Position {
   public int searchInsert(int[] arr, int target) {
     int index = -1;
 
-    if (target == 0) {
-      index = 0;
-    } else if (target > arr[arr.length - 1]) {
-      index = arr.length;
-    } else {
-      for (int i = 0; i < arr.length; i++) {
-        if (arr[i] == target) {
-          index = i;
-          break;
-        } else if (target > arr[i]) {
-          index = i + 1;
-        } else if (i == 0 && target < arr[i]) {
-          index = 0;
-        }
-      }
+    int start = 0;
+    int end = arr.length - 1;
 
+    while (start <= end) {
+      int mid = (start + end) / 2;
+
+      if (arr[mid] == target) {
+        index = mid;
+        break;
+      } else if (target > arr[mid]) {
+        start = mid + 1;
+      } else if (target < arr[mid]) {
+        end = mid - 1;
+      }
     }
 
-    return index;
+    return index != -1 ? index : start;
   }
 }

39b655b7a92728749b79ec8e182a47bf08fe8a0f
Shaan Alam <shaanalam369@gmail.com>
2021-09-30 13:04:01 +0530

    updated search in a rotated array question

--- arrays/medium/Search_In_Rotated_Array.java
+++ arrays/medium/Search_In_Rotated_Array.java
@@ -6,15 +6,55 @@ package medium;
 
 public class Search_In_Rotated_Array {
   public int search(int[] nums, int target) {
-    int result = -1;
+    int pivot = findPivot(nums);
+    int start = 0;
 
-    for (int x = 0; x < nums.length; x++) {
-      if (nums[x] == target) {
-        result = x;
-        break;
+    System.out.println("Peak" + pivot);
+    int index = binarySearch(nums, start, pivot, target);
+    if (index == -1) {
+      index = binarySearch(nums, pivot + 1, nums.length - 1, target);
+    }
+    return index != -1 ? index : -1;
+  }
+
+  public int binarySearch(int[] arr, int start, int end, int target) {
+
+    while (start <= end) {
+      int mid = (start + end) / 2;
+      if (target > arr[mid]) {
+        start = mid + 1;
+      } else if (target < arr[mid]) {
+        end = mid - 1;
+      } else {
+        return mid;
+      }
+
+    }
+
+    return -1;
+  }
+
+  public static int findPivot(int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+
+    while (start <= end) {
+      int mid = (start + end) / 2;
+
+      if (mid < end && arr[mid] > arr[mid + 1]) {
+        return mid;
+      }
+      if (mid > start && arr[mid] < arr[mid - 1]) {
+        return mid - 1;
+      }
+
+      if (arr[mid] <= arr[start]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
       }
     }
 
-    return result;
+    return -1;
   }
 }

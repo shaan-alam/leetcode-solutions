801987f2d6b762ba091ed16dd1adee4f8027f520
Shaan Alam <shaanalam369@gmail.com>
2021-09-30 13:02:45 +0530

    updated search in a rotated array II question with binary search solution

--- arrays/medium/Search_In_Rotated_Array_II.java
+++ arrays/medium/Search_In_Rotated_Array_II.java
@@ -5,13 +5,68 @@ package medium;
 */
 
 public class Search_In_Rotated_Array_II {
-  public boolean search(int[] nums, int target) {
-    for (int x : nums) {
-      if (x == target) {
-        return true;
+  public boolean search(int[] arr, int target) {
+    int start = 0;
+    int pivot = findPivot(arr);
+
+    System.out.println(pivot);
+    int index = binarySearch(arr, start, pivot, target);
+    if (index == -1) {
+      index = binarySearch(arr, pivot + 1, arr.length - 1, target);
+    }
+
+    return index == -1 ? false : true;
+  }
+
+  static int binarySearch(int[] arr, int start, int end, int target) {
+
+    while (start <= end) {
+      int mid = (start + end) / 2;
+
+      if (arr[mid] == target) {
+        return mid;
+      } else if (target > arr[mid]) {
+        start = mid + 1;
+      } else if (target < arr[mid]) {
+        end = mid - 1;
       }
     }
 
-    return false;
+    return -1;
+  }
+
+  static int findPivot(int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+
+    while (start <= end) {
+      int mid = (start + end) / 2;
+
+      if (mid > start && arr[mid] < arr[mid - 1]) {
+        return mid - 1;
+      }
+
+      if (mid < end && arr[mid] > arr[mid + 1]) {
+        return mid;
+      }
+
+      if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+        if (start < end && arr[start] > arr[start + 1]) {
+          return start;
+        }
+        start++;
+
+        if (end > start && arr[end] < arr[end - 1]) {
+          return end - 1;
+        }
+        end--;
+      } else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+
+    return -1;
   }
 }
